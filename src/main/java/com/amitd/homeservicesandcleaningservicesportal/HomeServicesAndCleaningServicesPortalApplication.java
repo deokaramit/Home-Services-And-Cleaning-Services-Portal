d88420a7dcefd56100b697f58961c4cb3c70f3a9
deokaramit <amitdeokar@gmail.com>
2022-10-28 20:48:56 +0530

    Initial Commit

---
+++ src/main/java/com/amitd/homeservicesandcleaningservicesportal/HomeServicesAndCleaningServicesPortalApplication.java
@@ -0,0 +1,13 @@
+package com.amitd.homeservicesandcleaningservicesportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServicesAndCleaningServicesPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServicesAndCleaningServicesPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amitd/homeservicesandcleaningservicesportal/HomeServicesAndCleaningServicesPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amitd.homeservicesandcleaningservicesportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServicesAndCleaningServicesPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
